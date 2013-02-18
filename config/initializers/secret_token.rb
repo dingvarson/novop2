0b8638b0e743a295600fd56ab2044c92839a4237
ademir10 <ddsuportservice@gmail.com>
2013-02-18 19:51:32 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omini::Application.config.secret_token = '4910d8d4fbe97478f9956688ab38c2ba08431cd22b681722bb3e2f3fd96adddedb228b6e56849195954e6835d5f600e62ecd100535714aeac4d2c2bc83c68b9e'
