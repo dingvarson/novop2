c77f91046b0004d822b9a1427f7e9f41f219888a
ademir10 <ddsuportservice@gmail.com>
2013-03-05 12:57:52 -0300

    carregando clientes devendo

--- app/controllers/principals_controller.rb
+++ app/controllers/principals_controller.rb
@@ -1,5 +1,11 @@
 class PrincipalsController < ApplicationController
   def show
-    
-  end
+   
+   @cadclis = Cadcli.where("datavencto < ?", DateTime.now.utc)
+
+      respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @cadclis }
+end
+end
 end
