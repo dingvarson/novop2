1ec1b2e350f9589ca5cebb70ee24a3bf88e06dda
ademir10 <ddsuportservice@gmail.com>
2013-03-05 17:25:04 -0300

    agora zerou

--- app/controllers/principals_controller.rb
+++ app/controllers/principals_controller.rb
@@ -1,7 +1,9 @@
 class PrincipalsController < ApplicationController
   def show
-   
-   @cadclis = Cadcli.where("datavencto < ?", DateTime.now.utc)
+    #esta query serve para trazer todos os clientes que não efetuaram
+    # o pagamento da mensalidade e o status é diferente de SEM CONTRATO
+   @cadclis = Cadcli.where("datavencto < ?", DateTime.now.utc).where("status<>?", "SEM CONTRATO")
+  
 
       respond_to do |format|
       format.html # index.html.erb
