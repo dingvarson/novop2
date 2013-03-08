7fcf3ebbc7a9ab24a6f944930ed4bbbc50e01c99
ademir10 <ddsuportservice@gmail.com>
2013-03-08 18:45:59 -0300

    vai pdf

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -4,5 +4,5 @@ config.default_options = {
 :page_size => 'Legal',
 :print_media_type => true
 }
-config.root_url = "http://localhost" # Use only if your external hostname is unavailable on the server
+#config.root_url = "http://localhost" # Use only if your external hostname is unavailable on the server
 end
