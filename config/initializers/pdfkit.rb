46a39261d5ea50e30ec248f4a703734ce26233ff
ademir10 <ddsuportservice@gmail.com>
2013-03-08 18:08:22 -0300

    alteracao pdf

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,5 +1,5 @@
 PDFKit.configure do |config|
-config.wkhtmltopdf = 'C:/wkhtmltopdf/wkhtmltopdf.exe'
+config.wkhtmltopdf = 'bin/wkhtmltopdf/wkhtmltopdf.exe'
 config.default_options = {
 :page_size => 'Legal',
 :print_media_type => true
