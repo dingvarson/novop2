dd82d2bf1a12b98e947c4ec14df715a0d5faa16b
ademir10 <ddsuportservice@gmail.com>
2013-03-18 16:36:24 -0300

    devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,8 @@ Omini::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.action_mailer.default_url_options = { :host => 'dd2.heroku.com' }
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
