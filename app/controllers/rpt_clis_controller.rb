6e603f8e2d23b9e049dc39853f14c44866c7aa27
ademir10 <ddsuportservice@gmail.com>
2013-03-18 12:54:01 -0300

    acerto de layouts com render

--- app/controllers/rpt_clis_controller.rb
+++ app/controllers/rpt_clis_controller.rb
@@ -2,28 +2,28 @@ class RptClisController < ApplicationController
  
    
  #ACTION QUE TRAZ TODOS OS CLIENTES CADASTRADOS
-  layout 'rpt_cliente/rpt_fichacli', :except => :index
+  #layout 'rpt_cliente/rpt_fichacli', :except => :index
   def show
    @cadcli = Cadcli.find(params[:id])
-  respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @cadcli }
+   
+   render :layout => 'rpt_cliente/rpt_fichacli'   
+     
       
     
-   end
+   
   end
   
  #ACTION QUE TRAZ TODOS OS CLIENTES CADASTRADOS
-  layout 'rpt_cliente/rpt_clientes', :except => :show
+  #layout 'rpt_cliente/rpt_clientes', :except => :show
   
   def index
    @cadclis = Cadcli.order("nomecli").all
   
-      respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @cadclis }
+   @cadcliss = Cadcli.sum(:valor)
    
-    end
+   render :layout => 'rpt_cliente/rpt_clientes' 
+   
+    
    end
 
 
